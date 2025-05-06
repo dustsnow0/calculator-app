917f6614941fcc0e1f5485e33fe4f2c7a7c7c2fc
dustsnow0 <dustsnow3@gmail.com>
2025-05-06 14:32:35 +0900

    フォームの入力2

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-document.getElementById('form').addEventListener('click',function(){
-    document.querySelector('input').value = '123'
+// 数字ボタンを全部取得してイベントを付ける
+document.querySelectorAll('.num').forEach(button => {
+    button.addEventListener('click', function () {
+        document.querySelector('input').value += button.value;
+    });
+});
+
+//入力した内容を消去する
+document.querySelector('.clr').addEventListener('click',function(){
+    document.querySelector('input').value = '';
 });
